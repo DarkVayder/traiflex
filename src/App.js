d0b8d77929370f48f88a2f6daa815ccea61675ae
Rabiu Muhammad Nasir <111989601+DarkVayder@users.noreply.github.com>
2024-02-02 15:46:41 +0100

    first step in creating movies poster/images

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
+import React from 'react';
 import './App.css';
-
+import Row from './Row';
 function App() {
   return (
     <div className="App">
       
     <h1>Netflix</h1>
+    <Row title="NETFLIX ORIGINALS"/>
 
     </div>
   );
