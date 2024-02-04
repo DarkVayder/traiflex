06f52690b1834025c70f9600454b0221cb63fdd4
Rabiu Muhammad Nasir <111989601+DarkVayder@users.noreply.github.com>
2024-02-04 23:44:15 +0100

    issues

--- src/Row.js
+++ src/Row.js
@@ -1,29 +1,39 @@
 import React, { useEffect, useState } from 'react';
 import axios from 'axios';
+import instance from './axios';
+
+const base_url = "https://image.tmdb.org/t/p/original/";
 
 function Row({ title, fetchUrl }) {
   const [movies, setMovies] = useState([]);
 
   useEffect(() => {
     async function fetchData() {
-      const request = await axios.get(fetchUrl);
-      setMovies(request.data.results);
-      return request;
+      const request = await instance.get(fetchUrl);
+      //setMovies(request.data.results);
+      //return request;
+      console.log(request)
     }
     fetchData();
   }, [fetchUrl]);
 
-  console.log(movies);
+  console.table(movies);
+
+
 
   return (
-    <div className='row'>
+    <div className="row">
       <h2>{title}</h2>
       
-    <div className='row__posters'>
+      <div className="row__posters">
 
-    {movies.map()}
+      {movies.map(movie => (
+        
+        <img src={`${base_url}${movie.poster_path}`} alt={movie.name} />
+      
+      ))}
 
-    </div>
+      </div>
 
     </div>
   );
