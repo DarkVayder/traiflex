5f3e8735f183c540ff07b0e17d2764e5c647c18c
Rabiu Muhammad Nasir <111989601+DarkVayder@users.noreply.github.com>
2024-02-05 15:11:25 +0100

    Netflix Home page

--- src/Banner.js
+++ src/Banner.js
@@ -8,7 +8,7 @@ function Banner() {
   useEffect(() => {
     async function fetchData() {
       try {
-        const request = await axios.get(requests.fetchNetflixOriginals);
+        const request = await axios.get(requests.fetchActionMovies);
         setMovie(
           request.data.results[
             Math.floor(Math.random() * request.data.results.length)
