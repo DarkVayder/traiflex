8d9ecad2bcc6762a7db406080f4dcbd7cf4a26a3
Rabiu Muhammad Nasir <111989601+DarkVayder@users.noreply.github.com>
2024-02-05 01:24:39 +0100

    Commit 2

--- src/App.js
+++ src/App.js
@@ -10,7 +10,12 @@ function App() {
     <h1>Netflix</h1>
     <Row title="NETFLIX ORIGINALS" fetchUrl={requests.fetchNetflixOriginals} />
     <Row title="TRENDING MOVIES" fetchUrl={requests.fetchTrending} />
-
+    <Row title="Top Rated" fetchUrl={requests.fetchTopRated} /> 
+    <Row title="Action Movies" fetchUrl={requests.fetchActionMovies} />
+    <Row title="Comedy Movies" fetchUrl={requests.fetchComedyMovies} />   
+    <Row title="Horror Movies" fetchUrl={requests.fetchHorrorMovies} /> 
+    <Row title="Romance Movies" fetchUrl={requests.fetchRomanceMovies} />
+    <Row title="Documentaries" fetchUrl={requests.fetchDocumentaries} />
     </div>
   );
 }
--- src/Row.js
+++ src/Row.js
@@ -1,5 +1,6 @@
 import React, { useEffect, useState } from 'react';
 import axios from './axios';
+import "./Row.css";
 
 const base_url = "https://image.tmdb.org/t/p/original/";
 
@@ -27,7 +28,10 @@ function Row({ title, fetchUrl }) {
 
       {movies.map(movie => (
         
-        <img src={`${base_url}${movie.poster_path}`} alt={movie.name} />
+        <img 
+          key={movie.id}
+          src={`${base_url}${movie.poster_path}`} 
+          alt={movie.name} />
       
       ))}
 
