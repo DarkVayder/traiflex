3826387775b8542faa01358ab1bfd5b68f418f25
Rabiu Muhammad Nasir <111989601+DarkVayder@users.noreply.github.com>
2024-02-07 16:30:57 +0100

    Trailers

--- src/Row.js
+++ src/Row.js
@@ -3,6 +3,13 @@ import axios from './axios';
 import "./Row.css";
 import YouTube from 'react-youtube';
 import movieTrailer from 'movie-trailer';
+import PropTypes from 'prop-types';
+
+Row.propTypes = {
+  title: PropTypes.string.isRequired,
+  fetchUrl: PropTypes.string.isRequired,
+  isLargeRow: PropTypes.bool,
+};
 
 const base_url = "https://image.tmdb.org/t/p/original/";
 
