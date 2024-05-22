65f3a66ff1e4f1d7a34d39ac1d6547ec275fe543
Rabiu Muhammad Nasir <111989601+DarkVayder@users.noreply.github.com>
2024-05-22 14:30:21 -0700

    feat: Created a Login page and installed react-dom for routing

--- src/App.js
+++ src/App.js
@@ -1,32 +1,15 @@
-import React from 'react';
-import './App.css';
-import Row from './Row';
-import requests from './request';
-import Banner from './Banner';
-import "./Banner.css";
-import Nav from './Nav';
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Home from './Home';
+import Login from "./Login";
 
 function App() {
-
   return (
-    <div className="app">
-    <Nav />
-      
-    <Banner/>
-    <Row title="Netflix Originals" 
-    fetchUrl={requests.fetchNetflixOriginals} 
-    isLargeRow
-    />
-  
-    
-    <Row title="Trending Movies" fetchUrl={requests.fetchTrending} />
-    <Row title="Top Rated" fetchUrl={requests.fetchTopRated} /> 
-    <Row title="Action Movies" fetchUrl={requests.fetchActionMovies} />
-    <Row title="Comedy Movies" fetchUrl={requests.fetchComedyMovies} />   
-    <Row title="Horror Movies" fetchUrl={requests.fetchHorrorMovies} /> 
-    <Row title="Romance Movies" fetchUrl={requests.fetchRomanceMovies} />
-    <Row title="Documentaries" fetchUrl={requests.fetchDocumentaries} />
-    </div>
+    <BrowserRouter>
+      <Routes>
+        <Route index element={<Home />} />
+        <Route path='/login' element={<Login />} />
+      </Routes>
+    </BrowserRouter>
   );
 }
 
