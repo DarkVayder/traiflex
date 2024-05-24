4c13e98d7def1ad861b418d8f3776ec26004c704
Rabiu Muhammad Nasir <111989601+DarkVayder@users.noreply.github.com>
2024-05-24 10:23:47 -0700

    feat: installed react-toastify

--- src/App.js
+++ src/App.js
@@ -2,6 +2,8 @@ import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Home from './Home';
 import Login from "./Login";
 import { AuthListener } from "./AuthListener"; 
+import { ToastContainer, toast } from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css';
 
 const App = () => {
   return (
