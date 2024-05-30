53034efcffaa0b008f376058dd7876bde596b725
Rabiu Muhammad Nasir <111989601+DarkVayder@users.noreply.github.com>
2024-05-30 16:29:55 -0700

    feat: Styling Profile page

--- src/Profile.js
+++ src/Profile.js
@@ -13,12 +13,13 @@ const Profile = () => {
     <div className='Profile'>
       <Nav />
       <div className='profile_body'>
-        <h1>Edit My Profile</h1>
+        <h1>My Profile</h1>
         <div className='profile_details'>
           <img src={avatar} alt='Avatar' className='profile_avatar' />
           <div className='profile_info'>
             <h2>{user?.email}</h2>
             <div className="profile__sub">
+              <h3>Plans</h3>
               <button onClick = {() => auth.signOut()} 
               className="profile__logout">Log Out</button>
             </div>
