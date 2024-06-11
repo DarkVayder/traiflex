88d7b6f3c296c7ca9d29198e4d96c3928c623a36
Rabiu Muhammad Nasir <111989601+DarkVayder@users.noreply.github.com>
2024-06-11 15:53:20 -0700

    feat: Coded the contents of the Home components and ensured isScrolled props is working

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,6 +1,9 @@
 import React, { useState, useEffect } from 'react';
 import Nav from '../components/Nav';
-import requests from '../request';
+import backgroundImage from "../assets/hero_banner.jpg";
+import Movielogo from "../assets/hero_title.png";
+import { FaPlay } from "react-icons/fa";
+import { AiOutlineInfoCircle } from 'react-icons/ai';
 
 export default function Home() {
   const [isScrolled, setIsScrolled] = useState(false);
@@ -20,7 +23,19 @@ export default function Home() {
   return (
     <div className="app">
       <Nav isScrolled={isScrolled} />
-      
+      <img src={backgroundImage} alt="background" 
+      className="background-image" />  
+      <div className="logo">
+        <img src={Movielogo} alt="Movie Logo"/> 
+        <div className="buttons flex">
+          <button className="flex j-center a center">
+            <FaPlay /> Play 
+          </button>  
+          <button className="flex j-center a center">
+            <AiOutlineInfoCircle /> More Info
+          </button> 
+        </div> 
+      </div>   
     </div>
   );
 }
