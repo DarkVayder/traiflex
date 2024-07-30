6ca6c7f3165549009a1fd0b73fe8e5accc92ab06
Rabiu Muhammad Nasir <111989601+DarkVayder@users.noreply.github.com>
2024-07-30 13:28:56 +0100

    feat: improved error handing

--- src/pages/Player.jsx
+++ src/pages/Player.jsx
@@ -8,70 +8,86 @@ export default function Player() {
   const location = useLocation();
   const videoId = location.state?.videoId;
 
+  if (!videoId) {
+    return <ErrorContainer>No video ID found. Please try again.</ErrorContainer>;
+  }
+
   return (
     <Container>
-      <div className='player'>
-        <div className="back" onClick={() => navigate(-1)}>
+      <PlayerWrapper>
+        <BackButton onClick={() => navigate(-1)} aria-label="Go back">
           <BsArrowLeft />
-        </div>
-        <div className="video">
+        </BackButton>
+        <VideoWrapper>
           <iframe 
             src={`https://www.youtube.com/embed/${videoId}?autoplay=1`} 
             title="YouTube video player" 
             frameBorder="0" 
             allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" 
-            allowFullScreen>
-          </iframe>
-        </div>
-      </div>
+            allowFullScreen
+          />
+        </VideoWrapper>
+      </PlayerWrapper>
     </Container>
   );
 }
 
 const Container = styled.div`
-  .player {
-    position: relative;
-    width: 100%;
-    height: 100vh;
-    background-color: black;
-    display: flex;
-    flex-direction: column;
-    align-items: center;
-    justify-content: center;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  height: 100vh;
+  background-color: black;
+`;
 
-    .back {
-      position: absolute;
-      top: 1rem;
-      left: 1rem;
-      display: flex;
-      align-items: center;
-      cursor: pointer;
-      color: white;
-      font-size: 2rem;
-      svg {
-        margin-right: 0.5rem;
-      }
+const PlayerWrapper = styled.div`
+  position: relative;
+  width: 100%;
+  height: 100%;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: center;
+`;
 
-      @media (max-width: 768px) {
-        top: 0.5rem;
-        left: 0.5rem;
-        font-size: 1.5rem;
-      }
+const BackButton = styled.div`
+  position: absolute;
+  top: 1rem;
+  left: 1rem;
+  display: flex;
+  align-items: center;
+  cursor: pointer;
+  color: white;
+  font-size: 2rem;
 
-      @media (max-width: 480px) {
-        top: 0.25rem;
-        left: 0.25rem;
-        font-size: 1rem;
-      }
-    }
-
-    .video {
-      width: 100%;
-      height: 100%;
-      iframe {
-        width: 100%;
-        height: 100%;
-      }
-    }
+  @media (max-width: 768px) {
+    top: 0.5rem;
+    left: 0.5rem;
+    font-size: 1.5rem;
   }
+
+  @media (max-width: 480px) {
+    top: 0.25rem;
+    left: 0.25rem;
+    font-size: 1rem;
+  }
+`;
+
+const VideoWrapper = styled.div`
+  width: 100%;
+  height: 100%;
+  iframe {
+    width: 100%;
+    height: 100%;
+  }
+`;
+
+const ErrorContainer = styled.div`
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  height: 100vh;
+  color: white;
+  background-color: black;
+  font-size: 1.5rem;
 `;
